63cf44d9b02dd0ecdf712ebcda3bed85ede81f15
Avinash <sreethalam.avinash@gmail.com>
2022-10-25 07:42:30 +0530

    Update read_write_parquet_files.py

--- spark/pycharm/spark sql/read_write_parquet_files.py
+++ spark/pycharm/spark sql/read_write_parquet_files.py
@@ -1,32 +1,36 @@
 from pyspark.sql import SparkSession
 
-data=[
-    ("Harsh","IT","UP",60000,34,1000),
-    ("Prateek","IT","DL",75000,28,12000),
-    ("Ashok","FIN","HR",50000,50,5000),
-    ("Manish","IT","UK",90000,32,15000),
-    ("Anshika","IT","UP",65000,21,20000)
+data = [
+    ("Harsh", "IT", "UP", 60000, 34, 1000),
+    ("Prateek", "IT", "DL", 75000, 28, 12000),
+    ("Ashok", "FIN", "HR", 50000, 50, 5000),
+    ("Manish", "IT", "UK", 90000, 32, 15000),
+    ("Anshika", "IT", "UP", 65000, 21, 20000)
 ]
 
-columns=["name","dept","state","salary","age","bonus"]
+columns = ["name", "dept", "state", "salary", "age", "bonus"]
 
-spark=(SparkSession
-.builder
-.appName("read_write_parquet_files")
-.getOrCreate()
-)
+spark = (SparkSession
+         .builder
+         .appName("read_write_parquet_files")
+         .getOrCreate()
+         )
 
-df=spark.createDataFrame(data=data,schema=columns)
+df = spark.createDataFrame(data=data, schema=columns)
 df.printSchema()
 df.show(truncate=False)
 
-(df.write.format("parquet")
-.mode("overwrite")
-.option("compression","snappy")
-.save("/tmp/data/parquet/emp.parquet"))
+df.write.\
+    format("parquet").\
+    mode("overwrite").\
+    option("compression", "snappy").\
+    option("path", "/tmp/data/parquet/emp.parquet").\
+    save()
 
-par_df=(spark.read.format("parquet")
-.load("/tmp/data/parquet/emp.parquet"))
+par_df = spark.read.\
+    format("parquet").\
+    option("path", "/tmp/data/parquet/emp.parquet").\
+    load()
 
 par_df.createOrReplaceTempView("employee")
 spark.sql("SELECT * FROM employee WHERE salary >= 65000").show(truncate=False)
